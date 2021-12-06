224f6a417ea558bf1547d342e36f3c9a4e538728
Ana Gauna <amgauna@gmail.com>
2021-12-06 17:27:50 -0300

    Create config.php

---
+++ crud/config/config.php
@@ -0,0 +1,11 @@
+$config['hostname'] = "localhost";
+$config['dbuser'] = "dbuser";
+$config['dbpassword'] = "dbpassword";
+$config['dbname'] = "dbname";
+$config['sitetitle'] = "sitetitle";
+
+define('DB_NAME', 'test');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('TITLE', 'sitetitle');
