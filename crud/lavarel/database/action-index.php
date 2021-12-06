a0ecc52c6f7925a08a4a9ed7ce1c38b16b07f1b1
Ana Gauna <amgauna@gmail.com>
2021-12-06 18:25:51 -0300

    Update action-index.php

--- crud/lavarel/database/action-index.php
+++ crud/lavarel/database/action-index.php
@@ -1,5 +1,14 @@
+
 public function index()
 {
-    $customers = Customer::all();
+    $customers = Customer::paginate(12);
     return view('customers.grid', compact('customers'));
 }
+
+// Mas precisamos alterar a view grid para apresentar os botões link. 
+// Faça da seguinte maneira: logo um pouco antes da última linha 
+// @endsection, e também antes da div de fechamento (para não ficar feio), 
+// adicione:
+
+{{ $customers->links() }}
+
