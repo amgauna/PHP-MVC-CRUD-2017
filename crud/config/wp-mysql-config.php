814a799a460af1676acc78c9cdd44105cba07e1f
Ana Gauna <amgauna@gmail.com>
2021-12-06 17:19:49 -0300

    Create wp-mysql-config.php

---
+++ crud/config/wp-mysql-config.php
@@ -0,0 +1,9 @@
+// ** Configurações do MySQL - Você pode pegar estas informações com o serviço de hospedagem ** //
+/** O nome do banco de dados do WordPress */
+define('DB_NAME', 'nome_do_banco_de_dados_aqui');
+/** Usuário do banco de dados MySQL */
+define('DB_USER', 'nome_de_usuario_aqui');
+/** Senha do banco de dados MySQL */
+define('DB_PASSWORD', 'senha_aqui');
+/** Nome do host do MySQL */
+define('DB_HOST', 'localhost');
